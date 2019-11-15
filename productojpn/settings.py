aa30ba7bc02fbbe23039664bad340b4b068a0a52
SamuelOyarzo <z.copae@gmail.com>
2019-11-15 14:52:04 -0300

    Primera entrega Unidad 2 CRUD sección 002D

--- productojpn/settings.py
+++ productojpn/settings.py
@@ -121,9 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 IMG_ROOT = os.path.join(BASE_DIR, 'img')
 IMG_URL = '/img/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
